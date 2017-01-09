81097665c7802d73eb7f0169a2ae1687b95a026e
Justin James <digitaldrummerj@gmail.com>
2017-01-09 02:35:52 -0700

    refined the deployment

--- deploy.sh
+++ deploy.sh
@@ -9,29 +9,32 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+# Run our compile script
+ng build --prod -o ./dist --base-href "/angular2-getting-started/"
+
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO dist
-cd dist
+git clone $REPO ../out
+cd ../out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 git config user.name "Travis CI"
 git config user.email "cideploy@digitaldrummerj.me"
 cd ..
 
 # Clean out existing contents
-rm -rf dist/**/* || exit 0
+rm -rf out/**/* || exit 0
 
-# Run our compile script
-ng build --prod -o ./dist --base-href "/angular2-getting-started/"
 # ng test --watch=false        
 
 # Now let's go have some fun with the cloned repo
-cd dist
+cd angular2-getting-started
+mv dist/* ../out
 
+cd ../out
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}.  Travis build: $TRAVIS_BUILD_NUMBER"
+git commit -m "Deploy to GitHub Pages: ${SHA}. " #" Travis build: $TRAVIS_BUILD_NUMBER"
 
 
 # Now that we're all set up, we can push.
