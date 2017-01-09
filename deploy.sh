1ebf5c6bc640a35daefc6a3d1779729e938475a3
Justin James <digitaldrummerj@gmail.com>
2017-01-09 01:01:13 -0700

    updated script

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,16 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
@@ -31,39 +41,29 @@ rm -rf dist/**/* || exit 0
 
 cd dist
 git add .
-git commit -m "removing old"
+git commit -m "removing old files"
 
 
 # Run our compile script
 cd ..
-ng build --prod --base-href 
-ng test --watch=false
+ng github-pages:deploy --message "travis build $TRAVIS_BUILD_NUMBER"
 
 # Now let's go have some fun with the cloned repo
-cd dist
+#cd dist
 
 # If there are no changes (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+#if [ -z `git diff --exit-code` ]; then
     echo "No changes to the spec on this push; exiting."
     exit 0
-fi
+#fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}.  Travis build: $TRAVIS_BUILD_NUMBER"
+#git add .
+#git commit -m "Deploy to GitHub Pages: ${SHA}.  Travis build: $TRAVIS_BUILD_NUMBER"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+#git push $SSH_REPO $TARGET_BRANCH
 
 #source: https://github.com/domenic/zones/blob/master/deploy.sh
