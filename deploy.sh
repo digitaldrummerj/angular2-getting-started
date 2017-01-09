e3d3d78f5a2b338c20e70747b049bf80b33d25c1
Justin James <digitaldrummerj@gmail.com>
2017-01-09 02:16:15 -0700

    fix dir name

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,15 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO ../out
+git clone $REPO ../dist
 cd dist
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 git config user.name "Travis CI"
 git config user.email "cideploy@digitaldrummerj.me"
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
 rm -rf dist/**/* || exit 0
-
 
 # Run our compile script
 ng build --prod -o ./dist --base-href "/angular2-getting-started/"
